a9094b08075f02ad6b7564b346887acf5cdda8ef
Anna94652 <anna.homey@gmail.com>
2025-11-15 13:18:06 -0500

    Split dataset (task 1)

--- microbiome_test.py
+++ microbiome_test.py
@@ -18,7 +18,7 @@ def Load16SFastA(path, fraction = 1.0):
     # from a file, read in all sequences and store them in a dictionary
     # sequences can be randomly ignored for testing by adjusting the fraction
     random.seed(11)
-    
+
     infile = open(path, 'r')
     sequences_16s = {}
     c = 0
@@ -28,26 +28,33 @@ def Load16SFastA(path, fraction = 1.0):
             my_id = line[1:-1]
             if random.random() < fraction:
                 sequences_16s[my_id] = ""
-            
-            
+
+
         else:
             if my_id in sequences_16s:
                 sequences_16s[my_id] += line[:-1]
-    
-       
+
+
     return sequences_16s
+
+def split_dataset(seqs, lib_len, quer_len):
+    items = list(seqs.items())
+    random.shuffle(items)
+    library = dict(items[quer_len:(quer_len + lib_len)])
+    queries = dict(items[:quer_len])
+    return library, queries
 
 
 
 def ConvertLibaryToKmerSets(library, K=2):
-    
+
     new_lib = {}
     c = 0
     for k in library.keys():
         new_lib[k] = set()
-        
+
         # add your code here to build the k-mer set
-        
+
     return new_lib
 
 def JaccardIndex(s1, s2):
@@ -58,31 +65,33 @@ def JaccardIndex(s1, s2):
 def KmerMatch(sequence_kmer_set, library_kmer_set):
     best_score = 0.0
     best_match = None
-    
+
     #add your code here to find the best kmer match
-    
+
     return best_score, best_match
 
 
 def AlignmentMatch(sequence, library):
     best_score = -10000000000
     best_match = None
-    
+
     #add your code here to find the best match using alignment
-    
+
     return best_score, best_match
 if __name__ == "__main__":
    # stuff only to run when not called via 'import' here
-   
 
-  
-   fn = "bacterial_16s_genes.fa"
-   sequences_16s = Load16SFastA(fn, fraction = 1.0)
-   
-   
-   print ("Loaded %d 16s sequences." % len(sequences_16s))
-   
-   
-   kmer_16s_sequences = ConvertLibaryToKmerSets(sequences_16s, K=6)
-   
-   
+
+
+    fn = "bacterial_16s_genes.fa"
+    sequences_16s = Load16SFastA(fn, fraction = 0.015)
+
+
+    print("Loaded %d 16s sequences." % len(sequences_16s))
+    library, queries = split_dataset(sequences_16s, 200, 50)
+    print("Library of length %d, Queries of length %d" % (len(library), len(queries)))
+    print(queries)
+
+    kmer_16s_sequences = ConvertLibaryToKmerSets(sequences_16s, K=6)
+
+
